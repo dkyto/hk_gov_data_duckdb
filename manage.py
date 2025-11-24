997b39a8693d709a383f45af3e530c24d2022851
dkyto <40445499+dkyto@users.noreply.github.com>
2025-11-24 19:57:01 +0800

    modified:   manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,11 @@
 import duckdb
-con = duckdb.connect(database=':memory:')
-con.execute("SELECT 1 as tester")
+import os
+
+token = os.environ.get("MOTHERDUCK_TOKEN")
+if not token:
+	raise RuntimeError("MOTHERDUCK_TOKEN environment variable not set.")
+
+# Connect to MotherDuck using the token
+con = duckdb.connect(database='github_actions', config={'motherduck_token': token})
+con.execute("CREATE OR REPLACE TABLE test_table AS SELECT 1 AS tester")
 con.close()
