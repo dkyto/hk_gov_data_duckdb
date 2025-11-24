43c380ec3ca51977954ace6761f317ea9afdb855
dkyto <40445499+dkyto@users.noreply.github.com>
2025-11-24 21:11:20 +0800

    modified:   manage.py

--- manage.py
+++ manage.py
@@ -9,7 +9,7 @@ if not token:
 con = duckdb.connect(
     database='md:dto_base',
     config = {'motherduck_token': token} )
-con.sql(f"""CREATE OR REPLACE TABLE tbl_raw_retail_sales_value AS(
+con.sql("""CREATE OR REPLACE TABLE tbl_raw_retail_sales_value AS(
   with json_rows as(
     SELECT unnest(from_json(json("dataSet"),'["JSON"]')) entries FROM
     read_json('https://www.censtatd.gov.hk/api/get.php?id=620-67001&lang=en&param=N4KABGBEDGBukC4zAL4BpxQM7yaCEkAagIIAyA+gEoDKiYA2pgVFQIYDuFAshQBYBrACYUhkZmAC6GFsQDylAJIARABrU6SJi0LsuARiEAHCgEshADwoA7SDJ2QAmgHtHFQyYCkFLOJbSJYnIKFXVaem0HPXdjM0sbOwlCFzcPCm9fCUlMdExIIwBTACdTZzE8QKwAFzYiqvpIACYABn0ANla-MFzCcwa2loBaNoB2Zs77SAAbNmsAcwaC2xAUIA',COLUMNS={dataSet:VARCHAR})
